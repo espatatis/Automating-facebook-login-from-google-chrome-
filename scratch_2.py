de0f394e6b47f0dd826c67422b1d44fdfad8dd56
rohans-mike <42403165+rohans-mike@users.noreply.github.com>
2019-08-01 16:12:43 +0530

    Update scratch_2.py

--- scratch_2.py
+++ scratch_2.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 udriver = webdriver.Chrome()
 udriver.get('https://www.facebook.com/')
 user_id = udriver.find_element_by_id('email')
-user_id.send_keys('rohans.mike')
+user_id.send_keys('your username')
 pasword = udriver.find_element_by_id('pass')
-pasword.send_keys('nautankisala3')
-pasword.submit()
+pasword.send_keys('password')
+pasword.submit
 print(udriver.current_url)
